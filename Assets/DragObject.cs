cf4e515a03584ceaa455ad940a8670380b0cb80b
eab6863 <eab6863@gmail.com>
2018-02-27 23:05:36 -0500

    Added Movement and Drag scenes

---
+++ Assets/DragObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragObject : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform rectTransform;
+
+    [SerializeField]
+    Vector2 boardSize;
+
+    // Use this for initialization
+    void Start () {
+        boardSize = transform.parent.GetComponentInParent<RectTransform>().rect.size / 2f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerDown(BaseEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(BaseEventData eventData)
+    {
+        rectTransform.anchoredPosition = ((PointerEventData)eventData).position - boardSize;
+    }
+}
