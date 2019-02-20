e6d873032d2ff5a7a5577660e1bc7e1e560af3c7
Eric Baker <eab6863@gmail.com>
2019-02-19 23:31:22 -0500

    Created click example

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Image))]
+public class ChangeColor : MonoBehaviour
+{
+    [SerializeField]
+    Image image;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	public void NewColor()
+    {
+        image.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+}
