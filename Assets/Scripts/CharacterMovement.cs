1e6ad48570a28d384f7870e42034e8a23dde2d00
eabigm <eabigm@IGM2435-25>
2022-03-30 10:42:14 -0400

    Adding comments to the ChacacterMovement script

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class CharacterMovement : MonoBehaviour
 {
+    #region Fields
     Vector3 vehiclePosition;
 
     [SerializeField]
@@ -19,6 +20,7 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]
     Text playerInputLabel;
     const string k_PLAYER_INPUT_STR = "Player Input:\nX = {0}\nY = {1}";
+    #endregion
 
     // Use this for initialization
     void Start()
@@ -39,42 +41,66 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {
 #if !UNITY_EDITOR && UNITY_ANDROID
-        if(!GravitySensor.current.enabled)
+        if(!GravitySensor.current.enabled)                      // Turn on the Gravity Sensor on the phone if it turned off
         {
             InputSystem.EnableDevice(GravitySensor.current);
         }
 #endif
 
+        //  Display the value of the player input to the HUD
         if (playerInputLabel != null)
             playerInputLabel.text = string.Format(k_PLAYER_INPUT_STR, playerInput.x, playerInput.y);
 
+        //  Move the Chacter based on player input
         Move(playerInput);
     }
 
+    /// <summary>
+    /// Move the Charcater based on Input from the Player
+    /// </summary>
+    /// <param name="movement">The delta movement to apply to the Player</param>
     void Move(Vector3 movement)
     {
+        //  Calculate a new position that is frame rate independant
         vehiclePosition += movement * speed * Time.deltaTime;
 
+        //  Check if the new postion is off the screen
         WrapVechileAroundCamera();
 
+        //  Apply the final new position to the Character
         transform.position = vehiclePosition;
     }
 
+    /// <summary>
+    /// Called when there is a change to the any of the Inputs set in InputActionMap.
+    /// </summary>
+    /// <param name="input">Data from the Input Device (Vector2)</param>
     public void OnMove(InputValue input)
     {
+        //  Read the data from the Input System
         playerInput = input.Get<Vector2>();
     }
 
+    /// <summary>
+    /// Called when there is a change to the Orientation of the Input Device.
+    /// </summary>
+    /// <param name="input">Data from the Input Device (Vector3)</param>
     public void OnRotate(InputValue input)
     {
+        //  Read the data from the Input System
         mobileGravity = input.Get<Vector3>();
 
+        //  Find the amount of difference off of the set baseline axes
         playerInput.x = Vector3.Dot(Vector3.right, mobileGravity);
         playerInput.y = Vector3.Dot(Vector3.up, mobileGravity);
 
+        //  Limit how far the Input Device needs to be Rotated
         Vector2.ClampMagnitude(playerInput, maxMobileRotationMag);
     }
 
+    /// <summary>
+    /// Wrap the Charcter around the screen if they go past the edge of the screen
+    /// </summary>
     void WrapVechileAroundCamera()
     {
         //  Check if off the x side of screen
@@ -98,6 +124,9 @@ public class CharacterMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Move the Character back to it's starting Position
+    /// </summary>
     public void Reset()
     {
         vehiclePosition = Vector3.zero;
